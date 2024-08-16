d8da988ebe555b2638c150c79199ed01e4c9df2b
MuhammadHassanminhas <muhammadhassan1762005@gmail.com>
2024-08-16 16:10:09 +0500

    pickling

--- Code/interface.py
+++ Code/interface.py
@@ -5,11 +5,12 @@ import string
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
+import streamlit as st
 stemmer = PorterStemmer()
 import pickle
 tfidf = pickle.load(open('vectorizer.pkl' , 'rb'))
 model = pickle.load(open('model.pkl' , 'rb'))
-
+st.title('SPAM OR HAM FILTER')
 def processing(text):
     text = text.lower()
     text = nltk.word_tokenize(text)
@@ -28,13 +29,15 @@ def processing(text):
         y.append(stemmer.stem(i))
     return ' '.join(y)
 
-text = input('Enter your text : ')
-
-transformed_text = processing(text)
+text =st.text_area('Enter your text : ')
+if st.button('Predict'):
+    transformed_text = processing(text)
 # print(model(transformed_text))
-vector_input = tfidf.transform([transformed_text])
-result = model.predict(vector_input)[0]
-if result==1:
-    print('spam')
-elif result==0:
-    print('not spam')
+    vector_input = tfidf.transform([transformed_text])
+    result = model.predict(vector_input)[0]
+    if result==1:
+     st.header('spam')
+    elif result==0:
+      st.header('not spam')
+
+
